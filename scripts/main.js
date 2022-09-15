e85801f557a31803b40e7bbc3ebdc6161b0bd229
9489yamamayumi <9489yamamayumi@gmail.com>
2022-09-15 18:16:16 +0900

    add css and js sample script

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector('p');
+myHeading.textContent = 'Hello world!';
+document.querySelector('html').addEventListener('click', function () {
+  alert('痛っ! つつかないで!');
+});
