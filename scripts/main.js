a1fb2ef13e092cc86b4ff059e47657427eb13443
9489yamamayumi <9489yamamayumi@gmail.com>
2022-09-15 23:19:14 +0900

    some practice of js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
-const myHeading = document.querySelector('p');
-myHeading.textContent = 'Hello world!';
-document.querySelector('html').addEventListener('click', function () {
-  alert('痛っ! つつかないで!');
-});
+const img = document.querySelector('.header-icon');
+
+img.onclick = () => {
+  const src = img.getAttribute('src');
+  if (src === 'images/icon.jpg') {
+    img.setAttribute('src','images/icon2.jpg');
+  } else {
+    img.setAttribute('src','images/icon.jpg');
+  }
+}
+
+let btn = document.querySelector('button');
+let dsc = document.querySelector('.description');
+
+function setUserName() {
+  const name = prompt('あなたの名前を入力してください。');
+  if(!name){
+    setUserName();
+  }else{
+    localStorage.setItem('name', name);
+    dsc.textContent = `Mozilla はかっこいいよ、${name}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  dsc.textContent = `Mozilla はかっこいいよ、${storedName}`;
+}
+
+btn.onclick = () => {
+  setUserName();
+}
